aaa15cb62844d54434a7f99def8d90480e583e43
DumithLusena <dumithlusena123@gmail.com>
2025-04-29 12:55:41 +0530

    create component and creat dashboart and set navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ViewEmployeeComponent } from './pages/view-employee/view-employee.component';
+import { AddEmployeeComponent } from './pages/add-employee/add-employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component:DashboardComponent
+    },
+    {
+        path:"dashboard",
+        component:DashboardComponent,
+        children:[
+            {
+                path:"view-employees",
+                component:ViewEmployeeComponent
+            },
+            {
+                path:"add-employee",
+                component:AddEmployeeComponent
+            }
+        ]
+    }
+];
